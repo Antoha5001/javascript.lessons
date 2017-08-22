d26ed97e640594c98f9a737d28b45688d1ae90ab
Antoha5001 <plantsvzzombi@mail.ru>
2017-08-22 19:18:24 +0700

    123

--- app/test2.js
+++ app/test2.js
@@ -1,4 +1,4 @@
-var text = "Lorem ipsum dolor sit amet.",
+/*var text = "Lorem ipsum dolor sit amet.",
     head = document.documentElement.childNodes[0],
     body = document.body,
     ul = body.children[body.children.length - 2],
@@ -15,4 +15,18 @@ if(!lastLi.firstChild){
     alert("Элемент пустой!");
 }
 for(var i = 0; i < tr.length; i++){
-    tr[i].children[i].style.backgroundColor = "red";}
+    tr[i].children[i].style.backgroundColor = "red";}*/
+
+var about = document.getElementsByTagName("h2")[0],
+    widget = document.getElementById("widget"),
+    dataWidget = widget.getAttribute("data-widget-name"),
+    link = document.getElementsByTagName("a");
+
+for (var i = 0; i < link.length; i++ ){
+    var href = link[i].getAttribute("href");
+    if(!href) continue;
+
+    if(href.indexOf("://") === -1 || href.indexOf("internal.com") != -1 )continue;
+
+    link[i].classList.add("external");
+}
