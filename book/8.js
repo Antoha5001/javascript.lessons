4fe28b89769f2dd93026097cf8a86c38fd229e96
Antoha5001 <plantsvzzombi@mail.ru>
2017-09-04 12:04:47 +0700

    132

--- book/8.js
+++ book/8.js
@@ -1,11 +1,36 @@
-"Use strict";
+// "Use strict";
 (function() {
 	var o = function () {
         
 	};
 	var f = function () {
-        console.log(Object.prototype.toString.call(this));
+       // console.log(Object.prototype.toString.call(this));
+		c.counter = 5;
+		function c() {
+            // console.log(arguments.callee);
+            console.log(c.counter);
+
+        }
+        c();
 	};
 	o.m = f;
 	o.m();
+
 })();
+var scope = "global scope";
+function check() {
+	var scope = "local scope";
+	function f() {
+        return scope;
+    }
+    return f;
+}
+function counter(n) {
+	return {
+		get count(){return n+n},
+		set count(m){
+			if (m >= n) n = m;
+			else throw new Error("Нельзя изменить");
+		}
+	};	
+};
