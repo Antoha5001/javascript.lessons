c49dfc571a89dd39dcc97497d369e84cef4651d8
Antoha5001 <plantsvzzombi@mail.ru>
2017-09-04 18:28:23 +0700

    132

--- book/9.js
+++ book/9.js
@@ -1,24 +1,7 @@
 "use strict";
-(function() {
+(function () {
 
 })();
-var a = {x:1};
 
-var b = Object.create(a);
-
-function Range(from, to) {
-	// var r = Object.create(range.methods);
-
-	this.from = from;
-	this.to = to;
-}
-Range.prototype = {
-	constructor:Range,
-	includes:123
-};
-//
-// range.methods = {
-// 	met1:1,
-
-// 	met2:2
-// };
+var A = {};
+var B = {};
