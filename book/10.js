99a6a5ffe8f9d195d812d8a6eb2c91d16c12c848
Antoha5001 <plantsvzzombi@mail.ru>
2017-09-07 17:26:46 +0700

    133

--- book/10.js
+++ book/10.js
@@ -2,5 +2,12 @@
 (function () {
 	"use strict";
 var pattern = /text/;
-console.log(pattern);
+// console.log(pattern);
 })();
+var p = document.getElementsByTagName("p")[0];
+var t = p.innerHTML;
+t = t.replace(/"([^"]*)"/g, "«$1»");
+p.innerHTML = t;
+console.log(p);
+// p = p.replace(/Текст/g,"123");
+console.log(t);
