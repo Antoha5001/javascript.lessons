3f52fb3bb98df9eecb3a045ce7b7ece3d9847ac7
Antoha5001 <plantsvzzombi@mail.ru>
2017-09-18 18:28:46 +0700

    form

--- book/15.js
+++ book/15.js
@@ -1,17 +1,41 @@
-
 "use strict";
 (function () {
 	window.onload = function () {
 
 
-
 	}
 })();
 
-var one = document.getElementsByClassName("one");
+function $_(el) {
+	if (document.getElementsByClassName(el)) return document.getElementsByClassName(el);
+	else if (document.querySelector(el)) return document.querySelector(el);
+}
+
+function scrollToTitle(el) {
+	Array.prototype.forEach.call(el, function (e) {
+		e.onclick = function () {
+			var targetUrl = e.getAttribute('href').slice(1),
+				target = document.querySelector('h2[data-title="' + targetUrl + '"]'),
+				targetCoord = target.getBoundingClientRect();
+
+			window.scrollTo(0, targetCoord.top);
+
+		}
+	});
+
+}
+
+var one = $_("one"),
+	h1 = $_("handle")[0],
+	link = document.getElementsByTagName("a");
 
 
-var firstDiv = one[0].getBoundingClientRect();
+var firstDiv = one[3].getBoundingClientRect();
 
+h1.onclick = function () {
+	window.scrollTo(0, firstDiv.top);
+}
 
+scrollToTitle(link);
 
+var form = document.getElementById("form");
