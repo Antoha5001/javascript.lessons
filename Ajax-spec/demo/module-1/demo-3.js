7e2770e06fa3fd665c4f99d24927d10acf2584c8
Antoha5001 <plantsvzzombi@mail.ru>
2017-10-30 10:53:29 +0700

    fhg

--- Ajax-spec/demo/module-1/demo-3.js
+++ Ajax-spec/demo/module-1/demo-3.js
@@ -9,15 +9,15 @@
 
 
 		// Показ текста, полученного из файла
-		function getText(fileName){
+		function getText(fileName) {
 			request = getXmlHttpRequest();
-			request.open('GET', fileName,true);
+			request.open('GET', fileName, true);
 			request.send(null);
 
 			request.onreadystatechange = function () {
-				if(request.readyState === 4){
-					if(request.status != 200){
-						console.log(request.status +": "+ request.statusText );
+				if (request.readyState === 4) {
+					if (request.status != 200) {
+						console.log(request.status + ": " + request.statusText);
 					}
 					else {
 						var p = document.createElement('p');
@@ -30,9 +30,29 @@
 		}
 
 		// Показ заголовков, полученных от сервера
-		function getInfo(fileName){
+		function getInfo(fileName) {
 			request = getXmlHttpRequest();
 			request.open('GET', fileName);
+
+			request.send(null);
+
+			request.onreadystatechange = function () {
+				if (request.readyState === 4) {
+					console.log(
+						"Размер файла: \t"+request.getResponseHeader('Content-Length')
+						+ "\n" +
+						"Файл изменён \t"+request.getResponseHeader('Last-Modified')
+						+ "\n" +
+						request.getAllResponseHeaders()
+					);
+				}
+				else {
+					var p = document.createElement('p');
+					p.innerHTML = request.responseText;
+					document.body.appendChild(p);
+				}
+
+			}
 
 		}
 
@@ -47,7 +67,6 @@
 		});
 
 	});
-
 
 
 })();
---
+++ Ajax-spec/labs/lab-1/lab-1.js
@@ -0,0 +1,26 @@
+(function () {
+	window.addEventListener('DOMContentLoaded', function () {
+
+			<!--
+		**
+		**	Задание 1. Создание объекта XmlHttpRequest
+		**
+	-->
+		var request;
+
+		/*
+	**
+	**	Задание 2. Выборка книги
+	**
+	*/
+
+
+		/*
+		**
+		**	Задание 3. Обработчик кнопки
+		**
+		*/
+
+
+	});
+})();
