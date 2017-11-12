3391fb158c573b253fd03ab0f2ba6543b62fb605
Antoha5001 <plantsvzzombi@mail.ru>
2017-11-13 00:29:21 +0700

    123

--- book/21.js
+++ book/21.js
@@ -9,6 +9,16 @@
 		link.addEventListener('click', function () {
 			document.body.appendChild(img);
 		});
+
+		var canvas = document.getElementById('canvas');
+		var c = canvas.getContext('2d');
+		c.beginPath();
+		c.moveTo(100,100);
+		c.lineTo(180,180);
+		c.lineTo(100,200);
+		c.lineTo(100,100);
+		c.stroke();
+		c.fill();
 	});
 
 })();
